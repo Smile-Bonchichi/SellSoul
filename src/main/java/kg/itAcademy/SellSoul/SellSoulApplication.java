a119ae819303b9d9a01d5df8d98343b867c1a1bc
Nurdin <ulanovnurdin@gmail.com>
2021-10-16 00:32:11 +0600

    init project

---
+++ src/main/java/kg/itAcademy/SellSoul/SellSoulApplication.java
@@ -0,0 +1,13 @@
+package kg.itAcademy.SellSoul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellSoulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SellSoulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kg/itAcademy/SellSoul/SellSoulApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.itAcademy.SellSoul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellSoulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
